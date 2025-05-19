3523a817d73b2ac50f31018a86ad4eab880a4f24
Andrea-Sgura <andreasgura96@gmail.com>
2025-05-19 16:24:50 +0200

    [UPD] Creata componente Header + impostata struttura, importata in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import Header from './components/Header';
 
 function App() {
 
   return (
-    <></>
+    <>
+      <Header />
+    </>
   )
 }
 
