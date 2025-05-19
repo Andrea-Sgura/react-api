c55f58688e55f6f07b06be232d41e5ade944663e
Andrea-Sgura <andreasgura96@gmail.com>
2025-05-19 17:18:55 +0200

    [UPD] Inserite card con rispettive informazioni e impostato il setActors per creare il collegamento

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,7 +9,7 @@ const Main = () => {
     // Definisco la funzione che effettua la chiamata ajax
     const getActors = () => {
         axios.get("https://lanciweb.github.io/demo/api/actors/").then((resp) => {
-            console.log(resp.data);
+            setActors(resp.data)
         })
     }
 
@@ -18,7 +18,37 @@ const Main = () => {
     return (
         <div className="container">
             <div className="row">
-                <div className="col-12">Hello World</div>
+                <div className="col-12">
+                    <h2>Attori</h2>
+                </div>
+            </div>
+            <div className="row">
+                {actors.map((actor) => {
+                    return (
+                        <div className="col-12 col-md-6 col-lg-3">
+                            <div>
+                                <div className="card-image">
+                                    <img src={actor.image} className="img-fluid" />
+                                </div>
+                                <div className="card-info">
+                                    <h4>
+                                        {actor.name}
+                                    </h4>
+                                    <p>
+                                        <em>{actor.birth_year}</em>
+                                        {actor.nationality}
+                                    </p>
+                                    <p>
+                                        {actor.biography}
+                                    </p>
+                                    <p>
+                                        {actor.awards}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
             </div>
         </div>
     )
