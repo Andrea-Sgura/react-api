d5ce1a55069f5416b9dd0170dfc6b6acbde41655
Andrea-Sgura <andreasgura96@gmail.com>
2025-05-19 16:45:21 +0200

    [UPD] Definita variabile per gli attori, definita funzione che effettua chiamata ajax, e impostato useEffects, stampato risultato in console.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Main = () => {
+    // Definisco la variabile di stato degli attori
+    const [actors, setActors] = useState([]);
+
+    // Definisco la funzione che effettua la chiamata ajax
+    const getActors = () => {
+        axios.get("https://lanciweb.github.io/demo/api/actors/").then((resp) => {
+            console.log(resp.data);
+        })
+    }
+
+    useEffect(() => getActors(), []);
+
     return (
-        <div></div>
+        <div className="container">
+            <div className="row">
+                <div className="col-12">Hello World</div>
+            </div>
+        </div>
     )
 }
 
